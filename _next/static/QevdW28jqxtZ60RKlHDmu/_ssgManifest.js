ae11db08d23b07196bb7db719458eb49b4d517c8
Joshua Chapman <joshua.chapman@joshuachp.dev>
2022-05-24 17:55:07 +0200

    chore: update site

---
+++ _next/static/QevdW28jqxtZ60RKlHDmu/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fnotes","\u002Fposts","\u002F","\u002Fnotes\u002F[path]","\u002Fposts\u002F[path]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/wAy9sOnMpJFbEE6DwNF89/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F","\u002Fposts","\u002Fnotes","\u002Fnotes\u002F[path]","\u002Fposts\u002F[path]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
