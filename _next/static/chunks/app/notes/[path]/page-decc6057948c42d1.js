df44c561c3525f83adf9d8f408b15ce7ca9a0dfd
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-04-03 08:16:54 +0200

    fix: goat counter script

---
+++ _next/static/chunks/app/notes/[path]/page-decc6057948c42d1.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[790,563],{7876:function(e,r,n){"use strict";n.r(r),n.d(r,{CodeBlock:function(){return u}});var t=n(9268),o=n(6006);n(8074);let u=e=>{let{children:r,className:u,language:c}=e,l=(0,o.useRef)(null);c&&""!==c||(c="plaintext");let f="language-".concat(c),i=(0,o.useCallback)(()=>{if(!c||!l.current)return;let e=l.current;n.e(310).then(n.bind(n,4310)).then(r=>{let n=r.default;n.configure({ignoreUnescapedHTML:!0}),n.highlightElement(e)})},[l,c]);return(0,o.useEffect)(()=>{i()},[l,c,i]),(0,t.jsx)("pre",{ref:l,className:"".concat(f," ").concat(null!=u?u:""," overflow-auto rounded border-2 border-primary-900 p-4 dark:border-primary-50"),children:(0,t.jsx)("code",{children:r})})};r.default=u},2191:function(e,r,n){Promise.resolve().then(n.bind(n,7876)),Promise.resolve().then(n.t.bind(n,3619,23))},8074:function(){},3177:function(e,r,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=n(6006),o=Symbol.for("react.element"),u=Symbol.for("react.fragment"),c=Object.prototype.hasOwnProperty,l=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,f={key:!0,ref:!0,__self:!0,__source:!0};function i(e,r,n){var t,u={},i=null,s=null;for(t in void 0!==n&&(i=""+n),void 0!==r.key&&(i=""+r.key),void 0!==r.ref&&(s=r.ref),r)c.call(r,t)&&!f.hasOwnProperty(t)&&(u[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===u[t]&&(u[t]=r[t]);return{$$typeof:o,type:e,key:i,ref:s,props:u,_owner:l.current}}r.Fragment=u,r.jsx=i,r.jsxs=i},9268:function(e,r,n){"use strict";e.exports=n(3177)}},function(e){e.O(0,[619,667,961,744],function(){return e(e.s=2191)}),_N_E=e.O()}]);
